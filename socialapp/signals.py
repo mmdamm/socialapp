cc486df4692a0587a199dc938158dc85cc5a0309
mmadamm <122172059+mmadamm@users.noreply.github.com>
2023-11-01 23:17:15 +0330

    signal 2
    signal for delete post and send email

--- socialapp/signals.py
+++ socialapp/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import m2m_changed, post_delete
 from django.dispatch import receiver
 from .models import Post
+from django.core.mail import send_mail
 
 
 @receiver(m2m_changed, sender=Post.likes.through)
@@ -8,3 +9,11 @@ def user_like(sender, instance, **kwargs):
     instance.total_likes = instance.likes.count()
     instance.save()
 
+
+@receiver(post_delete, sender=Post)
+def delete_post(sender, instance, **kwargs):
+    author = instance.author
+    subject = f"Your post delete."
+    message = f"Your post with id({instance.id}) has been delete."
+    send_mail(subject, message, 'mohammad2547mohseny@gamil.com', [author.email],
+              fail_silently=False)
