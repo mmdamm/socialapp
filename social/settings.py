499954592860d3798544d31776bcf394b4898a21
mohammad <122172059+mmdamm@users.noreply.github.com>
2024-08-01 22:07:15 +0330

    Update settings.py

--- social/settings.py
+++ social/settings.py
@@ -168,7 +168,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mohammad2547mohseny@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'ibnytyrbhzbcvpzz'
+EMAIL_HOST_PASSWORD = ''
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
